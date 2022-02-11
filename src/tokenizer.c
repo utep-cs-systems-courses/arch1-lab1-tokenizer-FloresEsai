854458d6df32583c047a009532d4efd1fe52fe11
FloresEsai <eflores31@miners.utep.edu>
2022-02-11 12:18:40 -0700

    first commit test

---
+++ src/tokenizer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "history.h"
+#include "tokenizer.h"
+
+
+/* we take user input: a string, using a for loop we determine how many words
+ * are in the string, disregarding the white space chars, and echo the string
+ * given
+ */
+int main()
+{
+
+}
+
+int space_char(char c)
+{
+
+}
+
+int non_space_char(char c)
+{
+
+}
+
+char *word_start(char *str)
+{
+
+}
