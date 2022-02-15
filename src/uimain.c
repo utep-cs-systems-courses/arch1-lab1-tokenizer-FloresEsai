3167a8b3b8b7f5894ec96bf9c28f322acbe2bd2e
Esai Flores <eflores31@miners.utep.edu>
2022-02-14 19:32:21 -0700

    "addition of uimain file as well as organizing tokenizer.c"

---
+++ src/uimain.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Welcome please enter a string");
+
+  /* we ask for a string, store the string, then ask for a specified number
+   * this number corresponds to an action within our program such as space_char
+   */
+  char *user_input;
+  int string_size;
+
+  scanf("%s", &user_input);
+
+  string_user = (char*) malloc((string_size+1)*sizeof(char));
+}
