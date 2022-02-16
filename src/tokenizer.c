75ca40596e46400815411be915d53fe1bade8757
Esai Flores <eflores31@miners.utep.edu>
2022-02-16 15:11:09 -0700

    "tokenizer updates"

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -25,19 +25,31 @@ char *word_start(char *str)
 {
     /* returns the pointer to the first non-space char om the first word of the user input  */
     /* if the string contains no words we return the zero pointer */
+    while(*str == space_char){
+        if (*str == non_space_char){
+            // return the char at the current string pointer index
 
+        }
+    }
 }
 
 char *word_terminator(char *word)
 {
     /* returns the pointer to the char after the end of word */
+    while(*word != '\0'){
+        // if the next index of the word is the terminator
+        // we take the current index and return it as our word terminator
 
+    }
 }
 
 int count_words(char *str)
 {
     /* returns the number of words in the user input */
     int count = 0;
+    // while we are not at the string terminator
+    // we check when a word begins and ends
+    // and each time we increment the count and return it once we hit \0
     while (*str != '\0'){
         str = word_start(str);
         str = word_terminator(word:str);
