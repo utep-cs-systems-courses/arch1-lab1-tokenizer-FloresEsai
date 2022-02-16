ec89e1f28653499b3f151252ae4d4346cbe0cf5e
Esai Flores <eflores31@miners.utep.edu>
2022-02-16 16:00:17 -0700

    "Attempting to solve word_start and terminator"

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -23,12 +23,11 @@ int non_space_char(char c)
 
 char *word_start(char *str)
 {
-    /* returns the pointer to the first non-space char om the first word of the user input  */
+    /* returns the pointer to the first non-space char of the first word of the user input  */
     /* if the string contains no words we return the zero pointer */
-    while(*str == space_char){
+    while (*str != '\0'){
         if (*str == non_space_char){
-            // return the char at the current string pointer index
-
+            // here is where i am stuck
         }
     }
 }
@@ -39,6 +38,7 @@ char *word_terminator(char *word)
     while(*word != '\0'){
         // if the next index of the word is the terminator
         // we take the current index and return it as our word terminator
+        // here i am also stuck
 
     }
 }
@@ -53,16 +53,32 @@ int count_words(char *str)
     while (*str != '\0'){
         str = word_start(str);
         str = word_terminator(word:str);
-        count ++;
+        count++;
     }
     return count;
 }
 
 char *copy_str(char *inStr, short len)
 {
-    /* returns a allocated zero terminated vector of allocated space-seperated tokens from zero terminated str */
-    // allocate space, traverse the string
-
+    /* returns a allocated zero terminated vector of allocated space-seperated tokens from zero terminated str
+    * we assign a pointer to the same address as inStr
+    * we create a new pointer and allocate the needed amount of space to create a new array of the same size
+    * while the pointer to the string is not currently the string terminator we set the current index
+    * of the to_be_copied to hold the same value as the current index of ptr
+    * increment each index of both arrays and once we are finished reset the pointer to the beginning of the array
+    * return the copied string */
+    char *ptr = inStr;
+    char *to_be_copied = (char *)malloc(sizeof(char)*len+1);
+    while (*ptr != '\0'){
+        *to_be_copied = *ptr;
+        ptr++;
+        to_be_copied++;
+    }
+    while (len > 0){
+        ptr--;
+        to_be_copied--;
+    }
+    return to_be_copied;
 }
 
 char **tokenize(char* str)
