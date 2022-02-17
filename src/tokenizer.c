bc53f9eb6eb7dbb373c1b4157bbeeb08b1660de8
Esai Flores <eflores31@miners.utep.edu>
2022-02-17 10:09:37 -0700

    "changed copy_str"

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -27,7 +27,7 @@ char *word_start(char *str)
     /* if the string contains no words we return the zero pointer */
     while (*str != '\0'){
         if (*str == non_space_char){
-            return *str;
+            return str;
         }
     }
 }
@@ -66,23 +66,15 @@ int count_words(char *str)
 
 char *copy_str(char *inStr, short len)
 {
-    /* returns a allocated zero terminated vector of allocated space-separated tokens from zero terminated str
-    * we assign a pointer to the same address as inStr
-    * we create a new pointer and allocate the needed amount of space to create a new array of the same size
-    * while the pointer to the string is not currently the string terminator we set the current index
-    * of the to_be_copied to hold the same value as the current index of ptr
-    * increment each index of both arrays and once we are finished reset the pointer to the beginning of the array
-    * return the copied string */
-    char *ptr = inStr;
-    char *to_be_copied = (char *)malloc(sizeof(char)*len+1);
-    while (*ptr != '\0'){
-        *to_be_copied = *ptr;
-        ptr++;
-        to_be_copied++;
-    }
-    while (len > 0){
-        ptr--;
-        to_be_copied--;
+     // returns a allocated zero terminated vector of allocated space-separated tokens from zero terminated str
+    char *string_to_be_copied = (char *)malloc(sizeof(char)*len+1);
+     // allocate space equal to the input string and +1 for the string terminator
+    while (*instr != '\0'){
+        char *string_to_be_copied = *inStr;
+     // we assign the current index of the string to the corresponding index of the new string array
+        inStr++;
+        string_to_be_copied++;
+     // we increment the pointers of both strings
     }
     return to_be_copied;
 }
