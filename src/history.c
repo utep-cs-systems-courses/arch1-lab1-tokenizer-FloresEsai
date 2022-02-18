1f374d8d3cca3712fa864911548c40d774b940e6
Esai Flores <eflores31@miners.utep.edu>
2022-02-17 23:00:19 -0700

    "Starting to implement the linked list data structure in order to implement history and its methods"

---
+++ src/history.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "history,h"
+
+// in order to implement the history method we will need a linked list data structure
+struct Node
+{
+    char[] data;           // string data
+    struct Node* next;     // pointer to next node
+}
+
+struct Node* newNode(char[] data)
+{
+    // allocate a new node in a linked list and using malloc set its data
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+
+    // set the .next pointer of the new node to point to null
+    node->next = NULL;
+
+    return node;
+}
+
+struct Node* construct_list()
+{
+    // construct three linked list nodes and rearrange pointers to construct a list
+    struct Node* one = NULL;
+    struct Node* two = NULL;
+    struct Node* three = NULL;
+
+    one = (struct Node*)malloc(sizeof(struct Node));
+    two = (struct Node*)malloc(sizeof(struct Node));
+    three = (struct Node*)malloc(sizeof(struct Node));
+
+    struct Node* head = one;
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    // return a pointer to the first node in the list
+    return head;
+}
+
