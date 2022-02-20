a42b59a4205b3904d9a1a1923b2fcbca3502a373
Esai Flores <eflores31@miners.utep.edu>
2022-02-20 14:20:03 -0700

    "Updates to tokenize methods: including free tokens, tokenize, and print tokens"

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,83 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "history.h"
 #include "tokenizer.h"
 
 int space_char(char c)
 {
     /* we check if the current char in the string is a white space */
-    if (char c == ' ' | char c == '\t' | char c == '\n'){
+    if (char c == ' ' | char c == '\t'){
         return true;
-    };
+    }
     return false;
-};
+}
 
 int non_space_char(char c)
 {
     /* we check if the current char in the string is not a white space */
-    if (char c != ' ' | char c != '\t' | char c != '\n'){
+    if (char c != ' ' | char c != '\t'){
         return true;
-    };
+    }
     return false;
-};
+}
 
 char *word_start(char *str)
 {
-    /* returns the pointer to the first non-space char of the first word of the user input  */
-    /* if the string contains no words we return the zero pointer */
-    while (*str != '\0'){
-        if (*str == non_space_char(*str)){
-            return str;
-        };
-    };
-};
+    int index;
+    for(index = 0; *(str + 1) != '\0'; index++;){
+        if(non_space_char(*(str + 1))){
+            return str + 1;
+        }
+    }
+    return str + 1; // if this line is reached then the beginning of the input is the start
+}
 
 char *word_terminator(char *word)
 {
-    /* returns the pointer to the char after the end of word
-    * if the string is empty we return NULL
-    * we then traverse the word incrementing the pointer and returning the last before the terminator
-    */
-    if(*word == '\0'){
-        printf("The string entered is empty\n");
-        return NULL;
-    };
-    while(*word){
-        word++;
-    };
-    word--;
-    return word
-};
+    int index;
+    for(index = 0; *(word + index) != '\0'; index++;){
+        if(space_char(index)){
+            return word + index;
+        }
+    }
+    return word + index;
+}
 
 int count_words(char *str)
 {
-    /* returns the number of words in the user input */
-    int count = 0;
-    // while we are not at the string terminator
-    // we check when a word begins and ends
-    // and each time we increment the count and return it once we hit \0
-    while (*str != '\0'){
-        str = word_start(str);
-        str = word_terminator(str);
-        count++;
-    };
-    return count;
-};
+    int count;                          // the total number of words in a string
+
+    char *beginning = word_start(str);  // the first char in a string using word_start
+
+    // we traverse the entire string and
+    // if we hit a non space char we know we have finished a word and update count
+    while(*beginning != '\0')
+    {
+        if(non_space_char(*beginning))
+        {
+            count += 1;
+        }
+        beginning = word_terminator(beginning); // if this line is reached we found the start of the word and can go to the end
+        beginning = word_start(beginning); // find the start of the next word
+    }
+    return count; // return the total number of words in the string input
+}
 
 char *copy_str(char *inStr, short len)
 {
-     // returns a allocated zero terminated vector of allocated space-separated tokens from zero terminated str
-    char *string_to_be_copied = (char *)malloc(sizeof(char)*len+1);
-     // allocate space equal to the input string and +1 for the string terminator
-    while (*instr != '\0'){
-        char *string_to_be_copied = *inStr;
-     // we assign the current index of the string to the corresponding index of the new string array
-        inStr++;
-        string_to_be_copied++;
-     // we increment the pointers of both strings
-    };
-    return to_be_copied;
-};
+    // allocate space corresponding to the length of the input string plus 1 (for the string terminator)
+    char *copy_of_string = malloc((len + 1) * sizeof(char));
+
+    int index; // loop index
+    for(index = 0; index < len; index++)
+    {
+        copy_of_string[index] = inStr[index]; // index by index we copy each item from the input into string copy
+    }
+    copy_of_string[index] = '\0'; // we create and add the string terminator of the copied string
+
+    return copy_of_string; // return the newly copied string
+}
 
 char **tokenize(char* str)
 {
@@ -85,9 +83,24 @@ char **tokenize(char* str)
     * we allocate memory using copy_string and with the created array assign
     * each index to be a word of the string
     */
-    while(str != '\0'){
-        word_start(str);
-        word_terminator(str);
-        copy_str(str, sizeof(str));
-    };
-};
+
+
+}
+
+void print_tokens(char **tokens)
+{
+    int index;
+    for(index = 0; tokens[index] != 0; index++;){
+        printf("%s\n", *(tokens));
+    }
+    printf(*tokens);
+}
+
+void free_tokens(char **tokens)
+{
+    int index;
+    for(index = 0; tokens[index] != 0; index++;){
+        free(tokens[index]);
+    }
+    free(tokens[index]);
+}
